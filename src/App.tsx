d1508a6906af50a58d8013277117705ffbce0afa
EmptyEXOT <sadullaev.alim@mail.ru>
2023-01-30 23:36:07 +0300

    dark and light themes + theme provider and hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React, {Suspense} from 'react';
-import './index.scss'
+import React, {Suspense, useContext, useState} from 'react';
+import './styles/index.scss'
 import {Link, Route, Routes} from "react-router-dom";
 import {AboutPageAsync} from "./pages/aboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/mainPage/MainPage.async";
+import {Theme, ThemeContext} from "./theme/ThemeContext";
+import {useTheme} from "./theme/useTheme";
+
+
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme();
     return (
-        <div className='app'>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>Switch theme</button>
             <Link to={'/'}>Main</Link>
             <Link to={'/about'}>About</Link>
             <Suspense fallback={<div>wait...</div>}>
