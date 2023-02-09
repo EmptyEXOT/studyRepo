62ba6649e91d6c05895ddefc2e3b419cc18fac5a
EmptyEXOT <sadullaev.alim@mail.ru>
2023-02-09 05:55:12 +0300

    webpack entry ext = tsx; App.tsx changes

---
+++ src/app/App.tsx
@@ -0,0 +1,18 @@
+import React, {Suspense} from 'react';
+import './styles/index.scss'
+import {classNames} from "shared/lib/classNames/classNames";
+import {useTheme} from "app/providers/ThemeProvider";
+import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
+const App = () => {
+    const {theme, toggleTheme} = useTheme();
+    return (
+        <div className={classNames('app', {}, [theme])}>
+            <Navbar />
+            <AppRouter/>
+            <button onClick={toggleTheme}>Switch theme</button>
+        </div>
+    );
+};
+
+export default App;
