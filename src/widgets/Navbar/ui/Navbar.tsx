97175fbc2947271fcaaa12faf265ab8c78372227
EmptyEXOT <sadullaev.alim@mail.ru>
2023-02-09 05:54:17 +0300

    Navbar widget

---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,21 @@
+import React, {FC} from 'react';
+import classes from './Navbar.module.scss'
+import {classNames} from "shared/lib/classNames/classNames";
+import AppLink, {AppLinkTheme} from "shared/ui/AppLink/AppLink";
+
+interface NavbarProps {
+    className?: string
+}
+
+const Navbar:FC<NavbarProps> = ({className}) => {
+    return (
+        <div className={classNames(classes.navbar, {}, [className])}>
+            <div className={classes.links}>
+                <AppLink theme={AppLinkTheme.PRIMARY} className={classes.link} to={'/'}>Main</AppLink>
+                <AppLink theme={AppLinkTheme.PRIMARY} className={classes.link} to={'/about'}>About</AppLink>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
