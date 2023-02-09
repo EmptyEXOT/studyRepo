7abf6e8897e87ea6c2f37ceab826f11b2b2dc4e7
EmptyEXOT <sadullaev.alim@mail.ru>
2023-02-09 05:53:02 +0300

    AppLink UI-kit element

---
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -0,0 +1,28 @@
+import {FC} from 'react'
+import classes from './AppLink.module.scss'
+import {classNames} from "shared/lib/classNames/classNames";
+import {Link, LinkProps} from "react-router-dom";
+
+export enum AppLinkTheme {
+    PRIMARY = 'primary',
+    SECONDARY = 'secondary',
+}
+
+interface AppLinkProps extends LinkProps{
+    className?: string,
+    theme?: AppLinkTheme
+}
+
+const AppLink:FC<AppLinkProps> = (props) => {
+    const {className, children, to, theme=AppLinkTheme.PRIMARY, ...otherProps} = props
+    return (
+        <Link to={to}
+              className={classNames(classes.appLink, {}, [className, classes[theme]])}
+              {...otherProps}
+        >
+            {children}
+        </Link>
+    );
+};
+
+export default AppLink;
