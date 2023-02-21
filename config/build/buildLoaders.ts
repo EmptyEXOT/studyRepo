456b971efaf6cc947547c41a00d4112d01c50083
EmptyEXOT <sadullaev.alim@mail.ru>
2023-02-21 23:44:18 +0300

    scss camelCase styleNames auto-convertation

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -55,6 +55,7 @@ export function buildLoaders({
                 loader: 'css-loader',
                 options: {
                     modules: {
+                        exportLocalsConvention: 'camelCase',
                         auto: (resPath: string) => Boolean(resPath.includes('.module.')),
                         localIdentName: isDev
                             ? '[path][name]__[local]--[hash:base64:5]'
