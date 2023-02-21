18901434251bbdd233fe2e397e68aaf9fd16a72d
EmptyEXOT <sadullaev.alim@mail.ru>
2023-02-21 21:23:58 +0300

    tests for classNames advanced lib

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,44 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+    test('some class', () => {
+        expect(classNames('someClass')).toBe('someClass');
+    });
+    test('with mods', () => {
+        expect(classNames('someClass', { pressed: true, blue: true })).toBe(
+            'someClass pressed blue',
+        );
+    });
+    test('with mods switched off', () => {
+        expect(classNames('someClass', { pressed: false, blue: true })).toBe(
+            'someClass blue',
+        );
+    });
+    test('with additional', () => {
+        expect(classNames('someClass', {}, ['red', 'large'])).toBe(
+            'someClass red large',
+        );
+    });
+    test('with additional and mods', () => {
+        expect(
+            classNames('someClass', { pressed: true, blue: true }, [
+                'red',
+                'large',
+            ]),
+        ).toBe('someClass red large pressed blue');
+    });
+    test('with equal mode name and additional class names', () => {
+        expect(classNames('', { red: true }, ['red', 'red'])).toBe('red');
+    });
+    test('empty className', () => {
+        expect(classNames('', { red: false }, ['  ', ''])).toBe('');
+    });
+    test('incorrect classNames filtering', () => {
+        expect(
+            classNames('Some class', { red: true }, [
+                ' Very Very   red',
+                'interesting    class  ',
+            ]),
+        ).toBe('someClass veryVeryRed interestingClass red');
+    });
+});
