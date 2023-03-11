ffc574ca1f33a716d3920f6b74a22b3086353ead
EmptyEXOT <sadullaev.alim@mail.ru>
2023-03-11 19:05:59 +0300

    loaders decomposition and storybook cfg

---
+++ config/storybook/webpack.config.ts
@@ -0,0 +1,28 @@
+import path from 'path';
+import webpack from 'webpack';
+import { IBuildPaths } from '../build/types/config';
+import { buildStyleLoader } from '../build/loaders/styleLoader';
+import { buildSvgLoader } from '../build/loaders/svgLoader';
+
+export default ({ config }: { config: webpack.Configuration }) => {
+    const paths: IBuildPaths = {
+        entry: '',
+        output: '',
+        html: '',
+        src: path.resolve(__dirname, '..', '..', 'src'),
+    };
+    // eslint-disable-next-line no-param-reassign
+    config.module.rules = config.module.rules.map(
+        (rule: webpack.RuleSetRule) => {
+            if (/svg/.test(rule.test as string)) {
+                return { ...rule, exclude: /\.svg$/i };
+            }
+            return rule;
+        },
+    );
+    config.module.rules.push(buildSvgLoader());
+    config.module.rules.push(buildStyleLoader(true));
+    config.resolve.modules.push(paths.src);
+    config.resolve.extensions.push('.ts', '.tsx');
+    return config;
+};
