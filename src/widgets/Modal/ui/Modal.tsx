e256f1f9bd8556c23adadc26c48105e1da5ac2fc
EmptyEXOT <sadullaev.alim@mail.ru>
2023-04-03 19:11:02 +0300

    Modal widget

---
+++ src/widgets/Modal/ui/Modal.tsx
@@ -0,0 +1,83 @@
+import {
+    FC,
+    MouseEvent,
+    ReactNode,
+    useCallback,
+    useEffect,
+    useRef,
+    useState,
+} from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import Portal from 'shared/ui/Portal/Portal';
+import { useTheme } from 'app/providers/ThemeProvider';
+import classes from './Modal.module.scss';
+
+interface ModalProps {
+    className?: string;
+    children?: ReactNode;
+    isOpen?: boolean;
+    onClose?: () => void;
+}
+
+const CLOSE_TIMEOUT: number = 300;
+
+const Modal: FC<ModalProps> = (props) => {
+    const { t } = useTranslation();
+    const { className, children, isOpen, onClose } = props;
+    const [isClosing, setIsClosing] = useState(false);
+    const timerRef = useRef<ReturnType<typeof setTimeout>>();
+    const { theme } = useTheme();
+
+    const closeHandler = useCallback(() => {
+        setIsClosing(true);
+        timerRef.current = setTimeout(() => {
+            onClose?.();
+            setIsClosing(false);
+        }, CLOSE_TIMEOUT);
+    }, [onClose]);
+
+    const onKeyDown = useCallback(
+        (e: KeyboardEvent) => {
+            if (e.key === 'Escape') {
+                closeHandler();
+            }
+        },
+        [closeHandler],
+    );
+
+    const onContentClick = (e: MouseEvent) => {
+        e.stopPropagation();
+    };
+
+    useEffect(() => {
+        if (isOpen) {
+            window.addEventListener('keydown', onKeyDown);
+        }
+
+        return () => {
+            clearTimeout(timerRef.current);
+            window.removeEventListener('keydown', onKeyDown);
+        };
+    }, [isOpen, onKeyDown]);
+
+    const mods: Record<string, boolean> = {
+        [classes.opened]: isOpen,
+        [classes.isClosing]: isClosing,
+        [classes[theme]]: true,
+    };
+
+    return (
+        <Portal>
+            <div className={classNames(classes.modal, mods, [className])}>
+                <div className={classes.overlay} onClick={closeHandler}>
+                    <div className={classes.content} onClick={onContentClick}>
+                        {children}
+                    </div>
+                </div>
+            </div>
+        </Portal>
+    );
+};
+
+export default Modal;
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,8 +1,8 @@
-import React, { FC } from 'react';
+import React, { FC, useCallback, useState } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
-import AppLink, { AppLinkVariant } from 'shared/ui/AppLink/AppLink';
-import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import { useTranslation } from 'react-i18next';
+import { Modal } from 'widgets/Modal';
+import Button, { ButtonVariant } from 'shared/ui/Button/Button';
 import classes from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -11,10 +11,28 @@ interface NavbarProps {
 
 const Navbar: FC<NavbarProps> = ({ className }) => {
     const { t } = useTranslation();
+    const [isAuthModal, setIsAuthModal] = useState(false);
+
+    const onToggleModal = useCallback(() => {
+        setIsAuthModal((prevState) => !prevState);
+    }, []);
 
     return (
         <div className={classNames(classes.navbar, {}, [className])}>
-            <div className={classes.links} />
+            <Button
+                variant={ButtonVariant.CLEAR_INVERTED}
+                className={classes.links}
+                onClick={onToggleModal}
+            >
+                {t('Войти')}
+            </Button>
+            {/* eslint-disable-next-line i18next/no-literal-string */}
+            <Modal isOpen={isAuthModal} onClose={onToggleModal}>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut
+                ducimus earum excepturi mollitia perspiciatis quis veniam!
+                Doloremque dolores eius, et id maiores neque omnis provident
+                quidem recusandae totam vero voluptates!
+            </Modal>
         </div>
     );
 };
