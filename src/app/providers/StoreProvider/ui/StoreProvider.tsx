446466259421f7b1f8d15f043a82be79572f6188
EmptyEXOT <sadullaev.alim@mail.ru>
2023-04-07 01:04:20 +0300

    Counter rtl tests

--- src/app/providers/StoreProvider/ui/StoreProvider.tsx
+++ src/app/providers/StoreProvider/ui/StoreProvider.tsx
@@ -1,5 +1,4 @@
 import { FC, ReactNode } from 'react';
-import { classNames } from 'shared/lib/classNames/classNames';
 import { Provider } from 'react-redux';
 import { createReduxStore } from 'app/providers/StoreProvider/config/store';
 import { StateSchema } from '../config/StateSchema';
