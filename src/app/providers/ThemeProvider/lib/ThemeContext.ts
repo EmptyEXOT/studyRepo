d33b1c36442a35494cda15a51c1105504bad0623
EmptyEXOT <sadullaev.alim@mail.ru>
2023-04-10 11:51:19 +0300

    global variables overriding fix

--- src/app/providers/ThemeProvider/lib/ThemeContext.ts
+++ src/app/providers/ThemeProvider/lib/ThemeContext.ts
@@ -1,13 +1,13 @@
 import { createContext } from 'react';
 
 export enum Theme {
-    DARK = 'dark',
-    LIGHT = 'light',
+    DARK = 'app_dark_theme',
+    LIGHT = 'app_light_theme',
 }
 
 export interface IThemeContextProps {
     theme?: Theme;
-    setTheme?: (theme: Theme) => void
+    setTheme?: (theme: Theme) => void;
 }
 
 export const ThemeContext = createContext<IThemeContextProps>({});
