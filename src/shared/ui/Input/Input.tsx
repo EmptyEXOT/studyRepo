fd843a63af410cddac9b6730dcbef9f457439cd6
EmptyEXOT <sadullaev.alim@mail.ru>
2023-04-16 22:33:58 +0300

    Input shared component modification

--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -1,6 +1,23 @@
-import React, { FC, InputHTMLAttributes } from 'react';
+import React, {
+    FC,
+    InputHTMLAttributes,
+    memo,
+    useEffect,
+    useRef,
+    useState,
+} from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import classes from './Input.module.scss';
+
+export enum InputVariant {
+    CLEAR = 'clear',
+    OUTLINED = 'outlined',
+}
+
+export enum InputColor {
+    TRANSPARENT = 'transparent',
+    LIGHT = 'light',
+}
 
 type HTMLInputProps = Omit<
     InputHTMLAttributes<HTMLInputElement>,
@@ -11,20 +28,57 @@ interface InputProps extends HTMLInputProps {
     className?: string;
     value?: string;
     onChange?: (value: string) => void;
+    variant: InputVariant;
+    color?: InputColor;
+    label?: string | undefined;
+    autofocus?: boolean;
 }
 
-const Input: FC<InputProps> = (props) => {
-    const { className, value, onChange, type = 'text', ...otherProps } = props;
+const Input = memo((props: InputProps) => {
+    const {
+        className,
+        value,
+        onChange,
+        type = 'text',
+        label,
+        variant,
+        color = InputColor.TRANSPARENT,
+        placeholder,
+        autofocus,
+        ...otherProps
+    } = props;
+
+    const [isFocused, setIsFocused] = useState(false);
+    const input = useRef<HTMLInputElement>();
+    useEffect(() => {
+        if (autofocus) {
+            input.current?.focus();
+        }
+    }, [autofocus]);
 
     const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
         onChange?.(e.target.value);
     };
 
     return (
-        <div className={classNames(classes.input, {}, [className])}>
-            <input type={type} value={value} onChange={onChangeHandler} />
+        <div className={classNames(classes.inputWrapper, {}, [className])}>
+            {label && <div className={classes.label}>{`${label}: `}</div>}
+            <div>
+                <input
+                    ref={input}
+                    className={classNames(classes.input, {}, [
+                        classes[variant],
+                        classes[color],
+                    ])}
+                    type={type}
+                    value={value}
+                    onChange={onChangeHandler}
+                    placeholder={placeholder}
+                    {...otherProps}
+                />
+            </div>
         </div>
     );
-};
+});
 
 export default Input;
